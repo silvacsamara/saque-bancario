41c142ce34cf224e421971a0bc418732aab7eb6f
silvacsamara <silvacsamara.dev@gmail.com>
2025-09-08 21:09:58 -0300

    Funcionalidade de saque

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+
+Escreva o **algoritmo** para que o caixa eletrônico funcione da seguinte forma:
+
+1. O usuário digita o valor que deseja sacar.
+2. O sistema verifica se o valor é múltiplo de 10 (pois só existem notas de R$10, R$20, R$50 e R$100).
+- Se **não for múltiplo de 10**, o sistema deve mostrar: *“Valor inválido. Digite um valor múltiplo de 10.”*
+- Se **for múltiplo de 10**, o sistema deve mostrar: *“Saque realizado com sucesso.”
+*/
+
+const prompt = require('prompt-sync')();
+
+let valorSaque = parseInt(prompt('Digite o valor que deseja sacar: '))
+
+if(valorSaque % 10 === 0){
+    console.log("Saque realizado com sucesso.")
+}else{
+    console.log("Valor inválido. Digite um valor múltiplo de 10.")
+}
